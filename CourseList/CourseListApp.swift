dbbb6b706c1b2d423c502b4ec414deb66b11d9dd
rricamar <r.ricamarti@gmail.com>
2022-06-24 11:31:43 +0200

    Initial Commit

---
+++ CourseList/CourseListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseListApp.swift
+//  CourseList
+//
+//  Created by Ruben Sopra on 24/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CourseListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
